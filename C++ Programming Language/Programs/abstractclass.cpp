074e9ca015bab319825a4df6fe2d723229ac37e0
Vignesh Ganesan <vicky08rockz@gmail.com>
2020-10-31 19:50:03 +0530

    added quiz answer key

---
+++ C++ Programming Language/Programs/abstractclass.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std; 
+  
+class Test 
+{ 
+   int x; 
+public: 
+    virtual void show() = 0; 
+    int getX() { return x; } 
+}; 
+  
+int main(void) 
+{ 
+    Test t; 
+    return 0; 
+} 
